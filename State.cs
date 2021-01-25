a6c9f9f6e225fbce4469b3ea8e5376e52a4dc5b9
diogo-h <diogo.m.henriques98@gmail.com>
2021-01-25 00:35:15 +0000

    Add State enumeration model

---
+++ State.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto3_LP2_2020.Common
+{
+    /// <summary>
+    /// This enumeration contains all the Piece states.
+    /// </summary>
+    public enum State
+    {
+        White,
+        Black,
+        Blocked
+    }
+}
