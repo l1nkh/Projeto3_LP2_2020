77911ba52bc4986cf80bf831adee909eb39fb054
diogo-h <diogo.m.henriques98@gmail.com>
2021-01-26 01:19:31 +0000

    Add enum with game states
    Made with João Dias and Pedro Fernandes

---
+++ GameState.cs
@@ -0,0 +1,11 @@
+namespace Projeto3_LP2_2020.Common
+{
+    public enum GameState
+    {
+        Menu,
+        TurnSelection,
+        SelectPiece,
+        SelectDirection,
+        VictoryScreen
+    }
+}
