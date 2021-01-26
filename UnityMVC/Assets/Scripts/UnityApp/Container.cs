dc9941c65afb909452ce92c4ba16e0341a89c9a2
diogo-h <diogo.m.henriques98@gmail.com>
2021-01-26 22:43:40 +0000

    Add unity basic setup and controller

---
+++ UnityMVC/Assets/Scripts/UnityApp/Container.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Projeto3_LP2_2020.Common;
+
+namespace Projeto3_LP2_2020.UnityApp
+{
+    [CreateAssetMenu(menuName = "Container")]
+    public class Container : ScriptableObject
+    {
+        public Board Board {get; private set;}
+        public Piece Piece {get; private set;}
+        public GameState Gamestate {get; private set;}
+        public Player Player {get; private set;}
+        public Position Position {get; private set;}
+        public State State {get; private set;}
+        public WinChecker WinChecker {get; private set;}
+        //meter aqui todas as classes que precisar mos para o viewer e controler
+
+        private void ONEnable()
+        {
+            Board = new Board();
+            Player = new Player();
+        }
+    }
+}
---
+++ UnityMVC/Assets/Scripts/UnityApp/Controller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Projeto3_LP2_2020.Common;
+
+namespace Projeto3_LP2_2020.UnityApp
+{
+    public class Controller : MonoBehaviour
+    {
+        
+    }
+}
