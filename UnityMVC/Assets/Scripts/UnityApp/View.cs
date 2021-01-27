47edfd2e43876e75f64c70ada4082eed35eeafd3
diogo-h <diogo.m.henriques98@gmail.com>
2021-01-27 00:43:44 +0000

    Add basic structure of the unity scene
    Made with Joao Dias

---
+++ UnityMVC/Assets/Scripts/UnityApp/View.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Projeto3_LP2_2020.UnityApp
+{
+    public class View : MonoBehaviour
+    {
+        /*[SerializeField] private 
+        void Awake()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+
+        private void OnEnable()
+        {
+
+        }
+
+        private void OnDisable()
+        {
+
+        }*/
+    }
+}
