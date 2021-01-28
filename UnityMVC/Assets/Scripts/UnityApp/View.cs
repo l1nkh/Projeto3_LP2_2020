89729867446e6d548a4c6114c56ceb549fda8bbc
FilipeVillar <filipevillar7@gmail.com>
2021-01-28 00:00:55 +0000

    Implemented skelleton of UnityMVC
    with Diogo Henriques

--- UnityMVC/Assets/Scripts/UnityApp/View.cs
+++ UnityMVC/Assets/Scripts/UnityApp/View.cs
@@ -1,22 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Projeto3_LP2_2020.UnityApp
 {
     public class View : MonoBehaviour
     {
-        /*[SerializeField] private 
+        [SerializeField] private Button button;
+        [SerializeField] private GameObject PieceNumber;
+        [SerializeField] private GameObject PieceState;
+        [SerializeField] private Controller controller;
+
+        private char buttonChar;
+
         void Awake()
         {
-            
         }
 
         void Update()
         {
-            
+            switch (PieceState.name)
+            {
+                case "None":
+                    buttonChar = ' ';
+                    PieceNumber.name = "";
+                    break;
+                case "Black":
+                    buttonChar = 'B';
+                    break;
+                case "White":
+                    buttonChar = 'W';
+                    break;
+                default:
+                    break;
+            }           
         }
-
+        private void FixedUpdate()
+        {
+            button.GetComponentInChildren<Text>().text = buttonChar + PieceNumber.name;
+        }
         private void OnEnable()
         {
 
@@ -25,6 +49,6 @@ namespace Projeto3_LP2_2020.UnityApp
         private void OnDisable()
         {
 
-        }*/
+        }
     }
 }
